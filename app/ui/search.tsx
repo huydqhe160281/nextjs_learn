9538f2547ae37fc154f6440cfd389fc086c5bb5a
huydqhe160281 <huydqhe160281@fpt.edu.vn>
2025-01-21 15:37:04 +0700

    update: add invoice

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,7 +2,7 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 interface SearchProps {
   placeholder: string;
@@ -12,6 +12,9 @@ export default function Search({ placeholder }: SearchProps) {
   const searchParams = useSearchParams();
   const pathName = usePathname();
   const { replace } = useRouter();
+  const [searchTerm, setSearchTerm] = useState<string>(
+    searchParams.get("query") || ""
+  );
 
   // Debounce function
   function debounce<T extends (...args: any[]) => void>(
@@ -25,7 +28,7 @@ export default function Search({ placeholder }: SearchProps) {
     } as T;
   }
 
-  const handleSearch = useCallback(
+  const updateSearchParams = useCallback(
     debounce((term: string) => {
       const params = new URLSearchParams(searchParams as any);
       params.set("page", "1");
@@ -35,10 +38,15 @@ export default function Search({ placeholder }: SearchProps) {
         params.delete("query");
       }
       replace(`${pathName}?${params.toString()}`);
-      console.log("searchParams", searchParams);
     }, 500),
     [searchParams, pathName, replace]
   );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    updateSearchParams(value);
+  };
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -49,7 +57,8 @@ export default function Search({ placeholder }: SearchProps) {
         id="search"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={handleChange}
+        value={searchTerm}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
